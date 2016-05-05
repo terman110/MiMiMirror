3d6014ef2557283e692a93bfc840178e4bfd716d
Jan <dev@janbeneke.de>
2016-05-05 20:22:02 +0200

    git pull extended

--- git_pull.php
+++ git_pull.php
@@ -1,6 +1,13 @@
 <?php
 echo '<h1>git pull</h1>';
-echo '<p>' . exec('git -C /home/pi/projects/MiMiMirror pull', $output, $return_val) . '</p>';
+echo '<p>' . exec('sudo git -C /home/pi/projects/MiMiMirror pull', $output, $return_val) . '</p>';
+echo '<p>';
+foreach($output as $out)
+	echo $out . '<br />';
+echo '</p>';
+echo '<p>' . $return_val . '</p>';
+echo '<p>---</p>'
+echo '<p>' . exec('sudo git -C /home/pi/projects/RPiMotionDetect pull', $output, $return_val) . '</p>';
 echo '<p>';
 foreach($output as $out)
 	echo $out . '<br />';
