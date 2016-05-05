afd5773b821b5fc2c325a34634fe8be641b7a7bd
Jan (Mirror) <dev@janbeneke.de>
2016-05-05 16:35:48 +0200

    cmd output

--- system_ip.php
+++ system_ip.php
@@ -1,4 +1,9 @@
 <?php
 echo '<p>ifconfig</p>';
-echo exec('ifconfig');
+echo '<p>' . exec('ifconfig', $output, $return_val) . '</p>';
+foreach($output as $out) 
+{
+	echo $out . '<br />';
+}
+echo '<p>' . $return_val . '</p>';
 ?>
