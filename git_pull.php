701174cc7e421a0bae13e737d61529b9efb88c31
Jan (Mirror) <dev@janbeneke.de>
2016-05-06 21:32:16 +0200

    ...

--- git_pull.php
+++ git_pull.php
@@ -1,16 +1,16 @@
 <?php
 echo '<h1>git pull</h1>';
-echo '<p>' . exec('sudo git -C /home/pi/projects/MiMiMirror pull', $output, $return_val) . '</p>';
+echo '<p>' . exec('git -C /home/pi/projects/MiMiMirror pull', $output, $return_val) . '</p>';
 echo '<p>';
 foreach($output as $out)
 	echo $out . '<br />';
 echo '</p>';
 echo '<p>' . $return_val . '</p>';
 echo '<p>---</p>'
-echo '<p>' . exec('sudo git -C /home/pi/projects/RPiMotionDetect pull', $output, $return_val) . '</p>';
+echo '<p>' . exec('git -C /home/pi/projects/RPiMotionDetect pull', $output2, $return_val2) . '</p>';
 echo '<p>';
-foreach($output as $out)
-	echo $out . '<br />';
+foreach($output2 as $out2)
+	echo $out2 . '<br />';
 echo '</p>';
-echo '<p>' . $return_val . '</p>';
+echo '<p>' . $return_val2 . '</p>';
 ?>
